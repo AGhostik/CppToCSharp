1e5a06fe9adc831848c9027d8bf3a50b8ece8b10
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2020-04-18 13:53:43 +0500

    Add project files.

---
+++ CosoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CosoleApp
+{
+    public class Program
+    {
+        [DllImport("CppDLL.dll")]
+        public static extern void hello_dll();
+
+        private static void Main()
+        {
+            Console.WriteLine("Hello from CSharp project!");
+
+            try
+            {
+                hello_dll();
+            }
+            catch (BadImageFormatException e)
+            {
+                Console.WriteLine($"ERROR!{Environment.NewLine}Set \"platform target\" = x86");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ CppDLL/MySource.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "cstdio"
+#include "MyHeader.h"
+
+void hello_dll()
+{
+    printf("Hello from Cpp project!");
+}
